bb1248c636102ed6eb9c8af64bb7fe30bfa042c2
danyel salazar <danygasia2@gmail.com>
2026-03-01 18:07:21 -0300

    add: empezando optimizacioj de register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,12 +4,6 @@ import { ChatContext } from "../context/ChatContext";
 
 
 const Register = () =>{
-    const [firstName, setFirsName] = useState("")
-    const [lastName, setLastName] = useState("")
-
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirnmPassword] = useState("")
     const [registrado, setRegistrado] = useState(false)
     const [error, setError] = useState(null)
     const [formData, setFormData] = useState({ firstName: ``, lastName: ``, email: ``, password: ``, confirmPassword: ``});
@@ -21,22 +15,22 @@ const Register = () =>{
     const handleSobmit = (e)=>{
         e.preventDefault()
 
-        if(password === confirmPassword){
+        if(formData.password === formData.confirmPassword){
             setRegistrado(true)
             setError(null)
             const newUser = {
                     id: crypto.randomUUID(),
-                    firstName: firstName,
-                    lastName: lastName,
+                    firstName: formData.firstName,
+                    lastName: formData.lastName,
                     address: { country: "VE" },
-                    email: email,
-                    password: password,
+                    email: formData.email,
+                    password: formData.password,
                     avatar: "https://media.licdn.com/dms/image/v2/D4D03AQFGlcLiV1xaDg/profile-displayphoto-scale_400_400/B4DZvJ55pmJoAg-/0/1768618958697?e=1772668800&v=beta&t=sKFzZ4fLEjzUCsd7oTePMKEkHh5nM58Fk_1F2Gnw6rA",
                     online: false,
                     lastMessage: "Hola soy un usuario registrado desde el formulario",
                     lastTime: "09:13",
                     messages: [
-                        { id: crypto.randomUUID(), author: firstName, text: "Hola soy un usuario registrado desde el formulario", time: "09:10" }
+                        { id: crypto.randomUUID(), author: formData.firstName, text: "Hola soy un usuario registrado desde el formulario", time: "09:10" }
                     ]
             }
             setUsers([...users, newUser])
@@ -44,36 +38,29 @@ const Register = () =>{
             // RESET: Volvemos al los valores iniociales del; formulario
             setFormData({ firstName: ``, lastName: ``, email: ``, password: ``, confirmPassword: `` }); 
             return
-        }else if(password != confirmPassword){
+        }else if(formData.password != formData.confirmPassword){
             setError(true)
         }
     }
 
     const handleChangeEmail = (e)=>{
-        setEmail(e.target.value)
         setFormData({...formData, email: e.target.value})
     }
 
     const handleChangePassword = (e)=>{
-        setPassword(e.target.value)
         setFormData({...formData, password: e.target.value})
     }
 
     const handleChangeConfirmPassword = (e)=>{
-        setConfirnmPassword(e.target.value)
         setFormData({...formData, confirmPassword: e.target.value})
     }
 
     const handleChangeFirsName = (e)=>{
-        setFirsName(e.target.value)
         setFormData({...formData, firstName: e.target.value})
     }
 
     const handleChangeLastName = (e)=>{
-        setLastName(e.target.value)
         setFormData({...formData, lastName: e.target.value})
-
-
     }
 
     return(
